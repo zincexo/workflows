1c40e4e34d171ccd72d2175ef44a27828bc32a1d
Sam <burrilldesign@gmail.com>
2015-02-22 09:57:37 +0000

    added coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp =require('gulp'),
-	gutil = require('gulp-util')
-;
-
-gulp.task('log', function(){
-	gutil.log('Workflows are awesome');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 	
-});
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+	});
+
+
