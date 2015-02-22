ae2946027c9c0f25ab3b96cdce8d3992bb8469bb
Sam <burrilldesign@gmail.com>
2015-02-22 12:42:56 +0000

    minfied html files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp =require('gulp'),
 	connect = require('gulp-connect'),
 	compass = require('gulp-compass'),
 	gulpif = require('gulp-if'),
-	uglify = require('gulp-uglify')
+	uglify = require('gulp-uglify'),
+	minifyHTML = require('gulp-minify-html')
 
 	;
 
@@ -77,7 +78,7 @@ gulp.task('watch', function(){
  gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html'])
+  gulp.watch('builds/development/*.html', ['html'])
    gulp.watch(jsonSources, ['json'])
 });
 
@@ -89,7 +90,9 @@ gulp.task('connect', function() {
 	});
 
 gulp.task('html', function(){
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env === 'production', minifyHTML()))
+	.pipe(gulpif(env === 'production',gulp.dest(outputDir)))
 	.pipe(connect.reload())
 
 	});
