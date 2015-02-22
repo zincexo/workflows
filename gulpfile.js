5fed14b3f9ed70e248381ae43d6cd451ff79d3c4
Sam <burrilldesign@gmail.com>
2015-02-22 10:44:11 +0000

    added

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp =require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat')
-	browserify = require('gulp-browserify')
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass')
 	;
 	
 	
@@ -12,6 +13,8 @@ var jsSources = ['components/scripts/rclick.js',
 				  'components/scripts/tagline.js',
 				  'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
+
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -29,3 +32,13 @@ gulp.task('js', function(){
 	
 	});
 
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+});
