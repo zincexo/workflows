7a3004de0bbcd707f976dc15499a26989d36fce1
Sam <burrilldesign@gmail.com>
2015-02-22 12:47:46 +0000

    crushed json filre

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp =require('gulp'),
 	compass = require('gulp-compass'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
-	minifyHTML = require('gulp-minify-html')
+	minifyHTML = require('gulp-minify-html'),
+	jsonminify = require('gulp-jsonminify')
 
 	;
 
@@ -79,7 +80,7 @@ gulp.task('watch', function(){
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('builds/development/*.html', ['html'])
-   gulp.watch(jsonSources, ['json'])
+   gulp.watch('builds/development/js/*.json', ['json'])
 });
 
 gulp.task('connect', function() {
@@ -98,7 +99,9 @@ gulp.task('html', function(){
 	});
 	
 gulp.task('json', function(){
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonminify()))
+	.pipe(gulpif(env === 'production',gulp.dest('builds/production/js')))
 	.pipe(connect.reload())
 
 	});	
