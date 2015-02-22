5ed07005e4b97303fa8ca247b7e19fb728e291d4
Sam <burrilldesign@gmail.com>
2015-02-22 10:12:48 +0000

    concats

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp =require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat')
+	;
+	
 	
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/rclick.js',
+				  'components/scripts/pixgrid.js',
+				  'components/scripts/tagline.js',
+				  'components/scripts/template.js'
+];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -11,4 +19,11 @@ gulp.task('coffee', function(){
 		.pipe(gulp.dest('components/scripts'))
 	});
 
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+	
+	
+	});
 
